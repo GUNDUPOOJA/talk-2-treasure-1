6f114a5c3f6f316abda176632e3fe11781509c25
gundu pooja <S538295@nwmissouri.edu>
2021-02-16 12:43:34 -0600

    added files

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,29 @@
-window.addEventListener('load', main);
-let synth = window.speechSynthesis;
+// window.addEventListener('load', main);
+// let synth = window.speechSynthesis;
 
-function main() {
-    console.log('Page is fully loaded');
-}
+// function main() {
+//     console.log('Page is fully loaded');
+// }
 
-async function onClickColor1() {
-    const diceValues = ["1", "2", "3", "4", "5", "6"];
-    const random = Math.floor(Math.random() * diceValues.length);
-    let diceValue = diceValues[random];
-    document.getElementById("randomValue1").innerHTML = diceValue;
-    let speech = new SpeechSynthesisUtterance(response.colorComponent1 + diceValue);
-    synth.speak(speech);
+// async function onClickColor1() {
+//     const diceValues = ["1", "2", "3", "4", "5", "6"];
+//     const random = Math.floor(Math.random() * diceValues.length);
+//     let diceValue = diceValues[random];
+//     document.getElementById("randomValue1").innerHTML = diceValue;
+//     let speech = new SpeechSynthesisUtterance(response.colorComponent1 + diceValue);
+//     synth.speak(speech);
 
-}
+// }
 
-async function onClickColor2() {
-    const diceValues = ["1", "2", "3", "4", "5", "6" ];
-    const random = Math.floor(Math.random() * diceValues.length);
-    let diceValue = diceValues[random];
-    document.getElementById("randomValue2").innerHTML = diceValue;
-    let speech = new SpeechSynthesisUtterance(response.colorComponent2 + diceValue);
-    synth.speak(speech);
+// async function onClickColor2() {
+//     const diceValues = ["1", "2", "3", "4", "5", "6" ];
+//     const random = Math.floor(Math.random() * diceValues.length);
+//     let diceValue = diceValues[random];
+//     document.getElementById("randomValue2").innerHTML = diceValue;
+//     let speech = new SpeechSynthesisUtterance(response.colorComponent2 + diceValue);
+//     synth.speak(speech);
     
-}
+// }
 import locationsArray from '../init-locations.js';
 
 let locationElement = document.getElementById("location");
