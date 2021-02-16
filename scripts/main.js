284c5921989b8ebe691edbe11e5078b81eb19172
gundu pooja <S538295@nwmissouri.edu>
2021-02-16 13:21:04 -0600

    edited main.js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,16 @@
-window.addEventListener('load', main);
-let synth = window.speechSynthesis;
 
-function main() {
-    console.log('Page is fully loaded');
-}
-
-async function onClickColor1() {
-    const diceValues = ["1", "2", "3", "4", "5", "6"];
-    const random = Math.floor(Math.random() * diceValues.length);
-    let diceValue = diceValues[random];
-    document.getElementById("randomValue1").innerHTML = diceValue;
-    let speech = new SpeechSynthesisUtterance(response.colorComponent1 + diceValue);
-    synth.speak(speech);
-
-}
-
-// async function onClickColor2() {
-//     const diceValues = ["1", "2", "3", "4", "5", "6" ];
-//     const random = Math.floor(Math.random() * diceValues.length);
-//     let diceValue = diceValues[random];
-//     document.getElementById("randomValue2").innerHTML = diceValue;
-//     let speech = new SpeechSynthesisUtterance(response.colorComponent2 + diceValue);
-//     synth.speak(speech);
-    
-// }
 import locationsArray from '../init-locations.js';
 
 let locationElement = document.getElementById("location");
+
 
 window.addEventListener('load', main);
 locationElement.addEventListener('click', locationHandler);
 locationElement.addEventListener('touch', locationHandler);
 
-// function main() {
-//     console.log('Page is fully loaded');
-// }
+function main() {
+    console.log('Page is fully loaded');
+}
 
 let currentlat;
 let currentlon;
