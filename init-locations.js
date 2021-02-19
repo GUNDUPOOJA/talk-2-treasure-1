029eb27a7cf23d5b81e6c3e561cfa4eaf335eb28
gundu pooja <S538295@nwmissouri.edu>
2021-02-19 08:56:55 -0600

    modified init-locations.js

--- init-locations.js
+++ init-locations.js
@@ -1,43 +1,80 @@
 export default [
-    {
-  Name: 'Horizons west apartments',
-   Latitude: 40.355951, 
-   Longitude: -94.882405,
-  // North: 40.35614, 
-  // South: 40.35614, 
-  // East: -94.88189,
-  // West:-94.88189,
-  //  South: 35.8425,
-},
-     {
-  Name: 'mooyah',
-  Latitude:40.35287,
-  Longitude:-98.88805,
-  // North: 40.35287, 
-  // West: -98.88805,
-  // South: 40.35287, 
-  // East: -98.88805,
-},
-{
-Name:'Bell Tower',
-Latitude:40.3398850339421, 
-Longitude:-94.8490055758143,
-},
-{
-Name:'Colden pond',
-Latitude:40.3398850339421, 
-Longitude:-94.8490055758143,
-},
-{
-Name:'Admin Block',
-Latitude:40.35339, 
-Longitude:-94.88357,
-},
-{
-Name:'B.D Owens Library',
-Latitude:40.35385, 
-Longitude:-94.88591,
+  {
+    Name: 'Horizons west apartments',
+    type: 'circle',
+    coordinates: [
+      {
+        Latitude: 40.355951,
+        Longitude: -94.882405
+      }
+    ],
+    radiusMeters: 30.0,
+    // North: 40.35614, 
+    // South: 40.35614, 
+    // East: -94.88189,
+    // West:-94.88189,
+    //  South: 35.8425,
+  },
+  {
+    Name: 'mooyah',
+    type: 'circle',
+    coordinates: [
+      {
+        Latitude: 40.35287,
+        Longitude: -98.88805,
+        // North: 40.35287, 
+        // West: -98.88805,
+        // South: 40.35287, 
+        // East: -98.88805,
+      }
+    ],
+    radiusMeters: 30.0,
+
+  },{
+    Name: 'Bell Tower',
+    type: 'circle',
+    coordinates: [
+      {
+    Latitude: 40.3398850339421,
+    Longitude: -94.8490055758143,
+  },
+], radiusMeters: 30.0,
+
 },
 
+  {
+    Name: 'Colden pond',
+    type: 'circle',
+    coordinates: [
+      {
+    Latitude: 40.3398850339421,
+    Longitude: -94.8490055758143,
+  },
+],
+radiusMeters: 30.0,
+
+},
+  {
+    Name: 'Admin Block',
+    type: 'circle',
+    coordinates: [
+      {
+    Latitude: 40.35339,
+    Longitude: -94.88357,
+  },
+],
+radiusMeters: 30.0,
+  },
+  {
+    Name: 'B.D Owens Library',
+    type: 'circle',
+    coordinates: [
+      {
+    Latitude: 40.35385,
+    Longitude: -94.88591,
+  },
+    ],
+    radiusMeters: 30.0,
+  },
 
 ];
